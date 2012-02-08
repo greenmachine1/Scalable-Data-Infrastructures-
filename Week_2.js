75e262a5c715a49a055e2e417b58f64643bfba0e
Cory Green <greenmachine@fullsail.edu>
2012-02-07 21:18:44 -0800

    Pseudocode of what needs to happen

--- Week_2.js
+++ Week_2.js
@@ -4,3 +4,79 @@
 // Project: Week 2
 // Class: Scalable Data Infrastructures
 
+// Project has a main function in which all the functions will come back to an return
+// a data type or do something.
+// Will start with pseudocode that will actually get filled afterwards
+
+Start
+declare variables
+
+procedure function
+boolean function
+number function
+string function
+array function
+
+returned values from those functions
+output
+and finish
+
+procedure function ( one argument to be passed )
+{
+	if the argument is true then
+	{	
+		do something;
+	}
+	else if the arguement is false then
+	{
+		do something else;
+	}
+
+
+return nothing;
+}
+
+boolean function ( 2 arguments to be passed )
+{
+	if both arguments are something then
+	{
+		do something if they are comparible;
+	}
+	else if
+	{
+		do something else if they are not comparible;
+	}
+
+return value;
+}
+
+number function ( one argument to be passed )
+{
+	declare local variables;
+	while (something is true)
+	{
+		do some math;
+		output some text;
+	}
+	
+return a value;
+}
+
+string function (2 string arguments to be passed)
+{
+	declare local variables;
+	string concatenation;
+	
+	return string;
+}
+
+array function ( 1 number and 1 array to be passed in)
+{
+	for loop;
+	{
+		do some math;
+		output that math;
+	}
+	return array values;
+}
+	
