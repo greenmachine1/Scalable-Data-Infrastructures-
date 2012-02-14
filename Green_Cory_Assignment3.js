9be8b965bab8fa164b494c84a7442a57a7e07d5a
Cory Green <greenmachine@fullsail.edu>
2012-02-14 02:54:38 -0800

    Basic layout of code in pseudocode

--- Green_Cory_Assignment3.js
+++ Green_Cory_Assignment3.js
@@ -1,1 +1,22 @@
-alert("JavaScript works!");
+// Cory Green
+// Week 3, Scalable Data Infrastructures
+// 2/14/2012
+// Project: Band Practice
+
+// pseudo-code for this weeks assignment
+// based off of flowchart that I will be designing
+
+main
+declare local variables
+
+function1();
+function2();
+function3();
+function4();
+function5();
+function6();
+
+return dataTypes;
+output thatData;
+
+end
