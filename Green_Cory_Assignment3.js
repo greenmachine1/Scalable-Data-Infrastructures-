ce30779666cf847acf67627ad7ef6bbd1fb82b88
Cory Green <greenmachine@fullsail.edu>
2012-02-15 14:49:03 -0800

    Addedfunction for going to practice and function for displaying band members

--- Green_Cory_Assignment3.js
+++ Green_Cory_Assignment3.js
@@ -3,20 +3,75 @@
 // 2/14/2012
 // Project: Band Practice
 
-// pseudo-code for this weeks assignment
-// based off of flowchart that I will be designing
 
-main
-declare local variables
 
-function1();
-function2();
-function3();
-function4();
-function5();
-function6();
+// Function for going to band practice
+var goingToPractice = function()
+{
+	console.log("Every Monday at 5:30, I leave my house to go to bandpractice");
+}
 
-return dataTypes;
-output thatData;
 
-end
+
+// displaying the band members!
+var bandPracticeMembers = function(bandMembers)
+{
+	for (var i = 0, j = bandMembers.length; i < j; i++)
+	{
+		console.log(bandMembers[i]);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// main program
+var bandMemberArray = ["Travis", "Kenny", "Mike", "Cory"];
+
+goingToPractice();
+console.log(bandPracticeMembers(bandMemberArray));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+// making object ball
+var ball = { 
+	size: 10, 
+	color: "red", 
+	lettering: "Green",
+	
+	sayHi: function ()
+	{
+		console.log("Hello!");
+	}
+};
+
+console.log(ball.size);
+console.log(ball.color);
+console.log(ball.lettering);
+console.log(ball.size * 3.1);
+
+
+
+ball.sayHi();
+*/
