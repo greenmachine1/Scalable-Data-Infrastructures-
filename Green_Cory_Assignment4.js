4cf99eebf92edf7e7bf507148b3bc6810190e738
Cory Green <greenmachine@fullsail.edu>
2012-02-23 11:24:11 -0800

    Removed testing function inputs

--- Green_Cory_Assignment4.js
+++ Green_Cory_Assignment4.js
@@ -39,10 +39,6 @@ var checkEmailAddress = function(eMail)
 {
 	var dotCom = (eMail.length - 4); 						// used to hold the length of the string minus 4 for the .com ending
 	
-	/*console.log(dotCom);			 						// displays the dotCom number
-	console.log(eMail.length + " " + eMail);				// displays the length + the email given
-	*/
-	
 	// decides whether or not the email has the required "." and also if the email has @.
 	// email.indexOf("@") means to look for the @ sign and if it is not there, to return -1
 	if ((eMail.substring(dotCom, (dotCom + 1)) === ".") && (eMail.indexOf("@") !== -1))				
@@ -57,11 +53,11 @@ var checkEmailAddress = function(eMail)
 }
 
 
-// 9, 7, 10, 90, 100, 2134
+
 // find the smallest number in an array
 var smallestNumberInArray = function(arraySet, number)
 {
-	var cache;									// have to run through the array once before going into the for loop
+	var cache;									
 	if(arraySet[0] < arraySet[1])
 	{
 		cache = arraySet[0];
@@ -115,75 +111,13 @@ var totalNumberOfArray = function(arrayNumbers)
 }
 
 
-var myUrl = "https://hello.com";
-var myUrl1 = "http//hello.com";
-var myUrl2 = "http://hello.com";
-
-var myPhone = "586-332-0001";
-var myPhone1 = "22123-45121";
-var myPhone2 = "221-4444443";
-
-var firstMonth = 4;
-var secondMonth = 5;
-var firstDay = 21;
-var secondDay = 18;
-
-
-var myArray = [9, 2, 10, 0, 100, 1];
-var numberForArray = 10;
-
-console.log(checkStringUrl(myUrl));
-console.log(checkStringUrl(myUrl1));
-console.log(checkStringUrl(myUrl2));
-
-console.log(checkPhoneNumber(myPhone));
-console.log(checkPhoneNumber(myPhone1));
-console.log(checkPhoneNumber(myPhone2));
-
-console.log(checkEmailAddress("Thingy_shouldnt_Be@hotmail.com"));
-console.log(checkEmailAddress("Thingy_shouldyahoo.com"));
-console.log(checkEmailAddress("This_is_not_An_email@this"));
-
-//differenceInDate(firstMonth, firstDay, secondMonth, secondDay);
-
-console.log(titleCase("hello there!"));
-console.log(titleCase("hi amanda!"));
-console.log(titleCase("no other"));
-
-console.log(totalNumberOfArray(myArray));
-
-console.log(smallestNumberInArray(myArray,numberForArray));
 
 
 
-/* objectives - Done! Does a string follow a 123-456-7890 pattern like a phone number?
 
-				Done! Does a string follow an aaa@bbb.ccc pattern like an email address?
+
 				
-				Done! Is the string a URL? (Does it start with http: or https:?)
-				
-				Done! Title-case a string (split into words, then uppercase the first letter of each word)
-				
-				Given a string that is a list of things separated by a given string, as well as another 
-				string separator, return a string with the first separator changed to the second: "a,b,c" 
-				+ "," + "/" → "a/b/c".
-				
-				Format a number to use a specific number of decimal places, as for money: 2.1 → 2.10
-				
-				Fuzzy-match a number: is the number above or below a number within a certain percent?
-				
-				Find the number of hours or days difference between two dates.
-				
-				Given a string version of a number such as "42", return the value as an actual Number, such as 42.
-				
-				Done! Find the smallest value in an array that is greater than a given number
-				
-				Done! Find the total value of just the numbers in an array, even if some of the items are not numbers.
-				
-				Given an array of objects and the name of a key, return the array sorted by the value of that key 
-				in each of the objects: "a" + [{a:2},{a:3},{a:1}] → [{a:1},{a:2},{a:3}].
-				
-*/
+
 
 
 
